5ba0449fcd1d2e46505d5cbcabccdbebc2f80e25
Bruno Henrique Saibert <brunosaibert@gmail.com>
2025-08-04 20:33:34 -0300

    feat: passing parameter to route

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,22 @@
-import { Text, View } from "react-native"
+import { router } from "expo-router"
+import { Button, Text, View } from "react-native"
 
 export default function Index() {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text>Expo Router</Text>
+      <Button
+        title="Nova Meta"
+        onPress={() => {
+          router.navigate("/target")
+        }}
+      />
+      <Button
+        title="Transação"
+        onPress={() => {
+          router.navigate("/transaction/123")
+        }}
+      />
     </View>
   )
 }
---
+++ src/app/transaction/[id].tsx
@@ -0,0 +1,18 @@
+import { router, useLocalSearchParams } from "expo-router"
+import { Button, Text, View } from "react-native"
+
+export default function Transaction() {
+  const params = useLocalSearchParams<{ id: string }>()
+
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Transaction ID: {params.id}</Text>
+      <Button
+        title="Voltar"
+        onPress={() => {
+          router.back()
+        }}
+      />
+    </View>
+  )
+}
