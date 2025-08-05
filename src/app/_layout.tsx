fa3e2d6a70fba457ad112fe820404c3d90dc4dc4
Bruno Henrique Saibert <brunosaibert@gmail.com>
2025-08-05 10:00:12 -0300

    feat: add expo fonts and loading component

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,8 +1,25 @@
 import { Stack } from "expo-router"
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_500Medium,
+  Inter_700Bold,
+} from "@expo-google-fonts/inter"
 
+import { Loading } from "@/components/Loading"
 import { colors } from "@/theme/colors"
 
 export default function RootLayout() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Inter_700Bold,
+  })
+
+  if (!fontsLoaded) {
+    return <Loading />
+  }
+
   return (
     <Stack
       screenOptions={{
